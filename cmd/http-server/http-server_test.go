7e833afdebf3fad41bb1e400b452e7346923760d
Alisher F <sambusa001@mail.ru>
2020-02-27 15:18:55 +0500

    initial commit

---
+++ cmd/http-server/http-server_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"net"
+	"strings"
+	"testing"
+)
+
+func Test_server(t *testing.T) {
+	go func() {
+		err := start("localhost:9999")
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	conn, err := net.Dial("tcp", "localhost:9999")
+	if err != nil {
+		t.Fatalf("can't connect to server: %v", err)
+	}
+	defer conn.Close()
+	writer := bufio.NewWriter(conn)
+	writer.WriteString("GET / HTTP/1.1\r\n")
+	writer.WriteString("Host: localhost\r\n")
+	writer.WriteString("\r\n")
+	writer.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "200 OK") {
+		t.Fatalf("non-success response: %s", response)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AlisherFozilov/http-file-server
+
+go 1.13
+
+// +heroku goVersion go1.13
